959a36713d2456a9713150a295115ec79c3504c5
Halden Kavanagh <haldenkav@gmail.com>
2024-01-21 21:59:06 -0700

    added bcrypt when you create an account

--- models/User.js
+++ models/User.js
@@ -28,6 +28,12 @@ User.init(
     },
   },
   {
+    hooks: {
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+    },
     sequelize,
     modelName: "user",
     freezeTableName: true,
