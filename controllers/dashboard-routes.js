1cf34e07318d449d05123baf300cf631513bb09e
Halden Kavanagh <haldenkav@gmail.com>
2024-01-24 17:16:10 -0700

    made a user's posts appear to the dashboard, when the edit post button is clicked directed to the view post page

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -9,7 +9,7 @@ router.get("/", async (req, res) => {
   } else {
     const allMyPost = await Post.findAll({
       where: {
-        user_id: req.session.id,
+        user_id: req.session.userId,
       },
       include: [
         {
@@ -34,32 +34,25 @@ router.get("/:id", async (req, res) => {
   if (!req.session.loggedIn) {
     res.redirect("/login");
   } else {
-    const aPost = await Post.findOne({
-      where: {
-        id: req.params.id,
-      },
-      include: [
-        {
-          model: Comment,
-        },
-      ],
-    });
+    const aPost = await Post.findByPk(
+      req.session.userId
+    );
 
-    const OnePost = aPost ? aPost.get({ plain: true }) : null;
+    const OnePost = aPost.get({ plain: true });
 
     try {
       res
         .status(200)
-        .render("dashboard", { OnePost, loggedIn: req.session.loggedIn });
+        .render("viewPost", { OnePost, loggedIn: req.session.loggedIn });
     } catch (err) {
       res.status(500).json(err);
     }
   }
 });
 
-// http://localhost:3001/dashboard/:id
+// http://localhost:3001/dashboard/update/:id
 //route to update a post
-router.put("/:id", (req, res) => {
+router.put("update/:id", (req, res) => {
   const requestedId = req.params.id;
   console.log(req.body);
   Post.findOneAndUpdate(
